5318624cf9b7506cd776b9095e2e1f6a50acfa2c
yuji128 <atomyuji@gmail.com>
2025-05-07 10:48:36 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Yuji")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(3)
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.StickFigure)
+})

938437b11a95bef99a670b8a9c7d7d5dc35bc3a5
yuji128 <atomyuji@gmail.com>
2025-05-07 11:04:21 -0400

    from school

--- main.ts
+++ main.ts
@@ -1,9 +1,17 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showString("Yuji")
-})
-input.onButtonPressed(Button.B, function () {
-    basic.showNumber(3)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 8) {
+        basic.showIcon(IconNames.Happy)
+        basic.pause(500)
+    } else {
+        basic.showIcon(IconNames.No)
+    }
 })
 basic.forever(function () {
-    basic.showIcon(IconNames.StickFigure)
+    radio.setGroup(1)
+    radio.sendNumber(8)
+    basic.pause(500)
+    led.plotBarGraph(
+    Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
+    9
+    )
 })

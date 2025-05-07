05be8b8e62436a402294e1b1aeb3b9f946fba3bc
yuji128 <atomyuji@gmail.com>
2025-05-07 11:45:23 -0400

    from school

--- main.ts
+++ main.ts
@@ -1,15 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 8) {
-        basic.showIcon(IconNames.Happy)
-        basic.pause(500)
-    } else {
-        basic.showIcon(IconNames.No)
+    if ((0 as any) == (8 as any)) {
+        while (true) {
+            basic.showIcon(IconNames.Happy)
+        }
     }
 })
+radio.setGroup(1)
 basic.forever(function () {
-    radio.setGroup(1)
     radio.sendNumber(8)
     basic.pause(500)
+    basic.showIcon(IconNames.No)
     led.plotBarGraph(
     Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
     9

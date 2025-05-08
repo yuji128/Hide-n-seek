d9385c8f2e36840e0bdce6c3cf27cffaf1de2682
yuji128 <atomyuji@gmail.com>
2025-05-08 11:50:20 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,34 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if ((0 as any) == (8 as any)) {
-        while (true) {
-            basic.showIcon(IconNames.Happy)
-        }
+    if (receivedNumber == myID) {
+        display = _2smiley
     }
 })
-radio.setGroup(1)
-basic.forever(function () {
+input.onButtonPressed(Button.A, function () {
+    display = _3dB
+})
+input.onButtonPressed(Button.B, function () {
     radio.sendNumber(8)
-    basic.pause(500)
-    basic.showIcon(IconNames.No)
+})
+let _3dB = 0
+let display = 0
+let _2smiley = 0
+let myID = 0
+radio.setGroup(1)
+myID = 8
+while (!(_2smiley)) {
+    let _1X = 0
     led.plotBarGraph(
     Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
     9
     )
+    if (display == _3dB) {
+        basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+    } else if (display == _1X) {
+        basic.showIcon(IconNames.No)
+    } else {
+    	
+    }
+}
+basic.forever(function () {
+    basic.showIcon(IconNames.Happy)
 })

830f2b37988edb0ad50f63cb58c45718109dd2d3
yuji128 <atomyuji@gmail.com>
2025-05-12 12:02:29 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,34 +1,53 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == myID) {
-        display = _2smiley
+    if (receivedNumber == 8) {
+        basic.showIcon(IconNames.Happy)
+    } else {
+    	
     }
+    counter = 0
 })
 input.onButtonPressed(Button.A, function () {
     display = _3dB
+    drawScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    radio.sendNumber(8)
+    display = _2smiley
+    drawScreen()
 })
-let _3dB = 0
-let display = 0
-let _2smiley = 0
-let myID = 0
-radio.setGroup(1)
-myID = 8
-while (!(_2smiley)) {
+function drawScreen () {
     let _1X = 0
-    led.plotBarGraph(
-    Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
-    9
-    )
-    if (display == _3dB) {
-        basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+    let _0graph = 0
+    if (display == _0graph) {
+        led.plotBarGraph(
+        Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
+        9
+        )
     } else if (display == _1X) {
         basic.showIcon(IconNames.No)
+    } else if (display == _2smiley) {
+        radio.sendNumber(8)
+        basic.pause(500)
+    } else if (display == _3dB) {
+        basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
     } else {
     	
     }
 }
+let _3dB = 0
+let display = 0
+let counter = 0
+let _2smiley = 0
+radio.setGroup(1)
+let myID = 8
+while (!(_2smiley)) {
+	
+}
+loops.everyInterval(500, function () {
+    counter += 1
+    if (counter) {
+    	
+    }
+})
 basic.forever(function () {
-    basic.showIcon(IconNames.Happy)
+	
 })

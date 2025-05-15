a2a28594857122fd6f8cf8a25c2b80ecf5ab0ab3
yuji128 <atomyuji@gmail.com>
2025-05-15 11:56:56 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,51 +1,53 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (receivedNumber == 8) {
-        basic.showIcon(IconNames.Happy)
+    if (receivedNumber == myID) {
+        display = _2smiley
+        drawScreen()
     } else {
-    	
+        counter = 0
     }
-    counter = 0
 })
 input.onButtonPressed(Button.A, function () {
     display = _3dB
     drawScreen()
 })
 input.onButtonPressed(Button.B, function () {
-    display = _2smiley
-    drawScreen()
+    radio.sendNumber(myID)
 })
 function drawScreen () {
-    let _1X = 0
-    let _0graph = 0
-    if (display == _0graph) {
-        led.plotBarGraph(
-        Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
-        9
-        )
-    } else if (display == _1X) {
-        basic.showIcon(IconNames.No)
-    } else if (display == _2smiley) {
-        radio.sendNumber(8)
-        basic.pause(500)
-    } else if (display == _3dB) {
-        basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+    if (display != _2smiley) {
+        if (display == _1X) {
+            basic.showIcon(IconNames.No)
+        } else if (display == _2smiley) {
+        	
+        } else if (display == _3dB) {
+            basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
+        } else {
+            led.plotBarGraph(
+            Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
+            9
+            )
+        }
+        display = _0graph
     } else {
-    	
+        basic.showIcon(IconNames.Happy)
     }
 }
+let _1X = 0
 let _3dB = 0
-let display = 0
 let counter = 0
 let _2smiley = 0
+let _0graph = 0
+let display = 0
+let myID = 0
 radio.setGroup(1)
-let myID = 8
-while (!(_2smiley)) {
-	
-}
+myID = 8
+display = _0graph
 loops.everyInterval(500, function () {
+    drawScreen()
     counter += 1
-    if (counter) {
-    	
+    if (counter >= 2) {
+        display = _1X
+        drawScreen()
     }
 })
 basic.forever(function () {

04ee65efff3642c07a4869fa36f3d7cede2df1bf
yuji128 <atomyuji@gmail.com>
2025-05-16 12:01:25 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,47 +1,58 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == myID) {
-        display = _2smiley
+        found = true
         drawScreen()
     } else {
         counter = 0
     }
 })
 input.onButtonPressed(Button.A, function () {
-    display = _3dB
-    drawScreen()
+    if (display == _0graph) {
+        display = _3dB
+        drawScreen()
+    } else if (display == _3dB) {
+        display = _0graph
+        drawScreen()
+    }
 })
 input.onButtonPressed(Button.B, function () {
     radio.sendNumber(myID)
 })
 function drawScreen () {
-    if (display != _2smiley) {
+    if (display == _2smiley) {
+        basic.showIcon(IconNames.Happy)
+    } else {
         if (display == _1X) {
             basic.showIcon(IconNames.No)
         } else if (display == _3dB) {
             basic.showNumber(radio.receivedPacket(RadioPacketProperty.SignalStrength))
-        } else {
+        } else if (display == _0graph) {
             led.plotBarGraph(
             Math.map(radio.receivedPacket(RadioPacketProperty.SignalStrength), -128, -28, 0, 9),
             9
             )
         }
         display = _0graph
-    } else {
-        basic.showIcon(IconNames.Happy)
     }
 }
-let _1X = 0
-let _3dB = 0
 let counter = 0
-let _2smiley = 0
+let found = false
 let _0graph = 0
 let display = 0
 let myID = 0
+let _2smiley = 0
+let _3dB = 0
+let _1X = 0
+_1X = 1
+_3dB = 3
+_2smiley = 2
 radio.setGroup(1)
 myID = 8
 display = _0graph
+found = false
 loops.everyInterval(500, function () {
-    if (display == _2smiley) {
+    if (found == true) {
+        display = _2smiley
         drawScreen()
     } else {
         display = _0graph
@@ -52,7 +63,4 @@ loops.everyInterval(500, function () {
             drawScreen()
         }
     }
-})
-basic.forever(function () {
-	
 })
